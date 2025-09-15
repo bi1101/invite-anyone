2dd76b5e5f1b900871a86fb74779e21329531543
bi1101 <bihungba1101@GMAIL.COM>
2025-09-15 18:25:25 +0700

    fix: add missing translation comments and ensure proper escaping in group invitation template

--- invite-link/templates/single-group-invite.php
+++ invite-link/templates/single-group-invite.php
@@ -17,7 +17,7 @@ get_header();
 	<main id="main" class="site-main">
 		<div>
 			<div>
-				<h1><?php printf( __( 'You have been invited to join: %s', 'invite-anyone' ), esc_html( $group->name ) ); ?></h1>
+				<h1><?php /* translators: %s: group name */ printf( esc_html( __( 'You have been invited to join: %s', 'invite-anyone' ) ), esc_html( $group->name ) ); ?></h1>
 				<?php if ( ! empty( $group->description ) ) : ?>
 					<div>
 						<p><?php echo esc_html( $group->description ); ?></p>
@@ -26,19 +26,19 @@ get_header();
 			</div>
 
 			<div style="margin-top: 30px;">
-				<a href="<?php echo wp_nonce_url( admin_url( 'admin-post.php?action=accept_group_invite&group_id=' . $bb_invite_group_id ), 'accept_group_invite_' . $bb_invite_group_id ); ?>"
+				<a href="<?php echo esc_url( wp_nonce_url( admin_url( 'admin-post.php?action=accept_group_invite&group_id=' . (int) $bb_invite_group_id ), 'accept_group_invite_' . (int) $bb_invite_group_id ) ); ?>"
 					class="button button-primary"
 					style="margin-right: 10px;">
-					<?php _e( 'Accept Invitation', 'invite-anyone' ); ?>
+					<?php esc_html_e( 'Accept Invitation', 'invite-anyone' ); ?>
 				</a>
-				<a href="<?php echo wp_nonce_url( admin_url( 'admin-post.php?action=reject_group_invite' ), 'reject_group_invite' ); ?>"
+				<a href="<?php echo esc_url( wp_nonce_url( admin_url( 'admin-post.php?action=reject_group_invite' ), 'reject_group_invite' ) ); ?>"
 					class="error">
-					<?php _e( 'Decline Invitation', 'invite-anyone' ); ?>
+					<?php esc_html_e( 'Decline Invitation', 'invite-anyone' ); ?>
 				</a>
 			</div>
 
 			<div style="margin-top: 30px;">
-				<p><?php printf( __( 'By accepting this invitation, you will become a member of %s.', 'invite-anyone' ), esc_html( $group->name ) ); ?></p>
+				<p><?php /* translators: %s: group name */ printf( esc_html( __( 'By accepting this invitation, you will become a member of %s.', 'invite-anyone' ) ), esc_html( $group->name ) ); ?></p>
 			</div>
 		</div>
 	</main>
